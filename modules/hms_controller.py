a63e8c0b148e2ba021657d817ff40a455270902c
dbsmith88 <dbsmith@uga.edu>
2018-04-05 10:44:55 -0500

    celery updates

--- modules/hms_controller.py
+++ modules/hms_controller.py
@@ -1,39 +1,44 @@
 from flask import Response
 from flask_restful import Resource, reqparse
-from celery import Celery
-from pymongo import MongoClient
-import redis
+import pymongo as pymongo
+from datetime import datetime
 import logging
 import json
-import os
+
+from flask_qed.celery_cgi import celery as celery
 
 # HMS Modules import
 from .hms.ncdc_stations import NCDCStations
 from .hms.percent_area import CatchmentGrid
 
-# envi setup
-from temp_config.set_environment import DeployEnv
-runtime_env = DeployEnv()
-runtime_env.load_deployment_environment()
-
-redis_hostname = os.environ.get('REDIS_HOSTNAME')
-redis_port = os.environ.get('REDIS_PORT')
-REDIS_HOSTNAME = os.environ.get('REDIS_HOSTNAME')
-redis_conn = redis.StrictRedis(host=REDIS_HOSTNAME, port=6379, db=0)
-
-if not os.environ.get('REDIS_HOSTNAME'):
-    os.environ.setdefault('REDIS_HOSTNAME', 'redis')
-    REDIS_HOSTNAME = os.environ.get('REDIS_HOSTNAME')
-
-logging.info("REDIS HOSTNAME: {}".format(REDIS_HOSTNAME))
-
-celery = Celery('flask_qed', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0', include=['flask_qed.hms_flask'])
-# celery = Celery('flask_qed', broker='redis://redis:6379/0', backend='redis://redis:6379/0', include=['flask_qed.hms_flask'])
-
-mongo = MongoClient('localhost', 27017)
+mongo = pymongo.MongoClient('localhost', 27017)
 mongo_db = mongo['flask_hms']
+mongo.flask_hms.Collection.create_index([("date", pymongo.DESCENDING)], expireAfterSeconds=86400)
+# ALL entries into mongo.flask_hms must have datetime.utcnow() timestamp, which is used to delete the record after 86400
+# seconds, 24 hours.
 
 parser = reqparse.RequestParser()
+
+
+class HMSTaskData(Resource):
+    """
+    Controller class to retrieve data from the mongoDB database and/or checking status of a task
+    """
+    parser.add_argument('job_id')
+
+    def get(self):
+        args = parser.parse_args()
+        task_id = args.job_id
+        if task_id is not None:
+            task = celery.AsyncResult(task_id)
+            if task.status == "SUCCESS":
+                posts = mongo_db.posts
+                stations = json.loads(posts.find_one({'_id': task_id})['data'])
+                return Response(json.dumps({'id': task.id, 'status': task.status, 'data': stations}))
+            else:
+                return Response(json.dumps({'id': task.id, 'status': task.status}))
+        else:
+            return Response(json.dumps({'error': 'id provided is invalid.'}))
 
 
 class NCDCStationsInGeojson(Resource):
@@ -53,23 +58,7 @@ class NCDCStationsInGeojson(Resource):
         job_id = self.start_async.apply_async(args=(geojson, args.startDate, args.endDate, args.crs), queue="qed")
         return Response(json.dumps({'job_id': job_id.id}))
 
-    parser.add_argument('job_id')
-
-    def get(self):
-        args = parser.parse_args()
-        task_id = args.job_id
-        if task_id is not None:
-            task = celery.AsyncResult(task_id)
-            if task.status == "SUCCESS":
-                posts = mongo_db.posts
-                stations = json.loads(posts.find_one({'_id': task_id})['data'])
-                return Response(json.dumps({'id': task.id, 'status': task.status, 'data': stations}))
-            else:
-                return Response(json.dumps({'id': task.id, 'status': task.status}))
-        else:
-            return Response(json.dumps({'error': 'id provided is invalid.'}))
-
-    @celery.task(name='hms_tasks', bind=True, ignore_result=False)
+    @celery.task(name='hms_ncdc_stations', bind=True, ignore_result=False)
     def start_async(self, geojson, start_date, end_date, crs):
         task_id = celery.current_task.request.id
         logging.info("task_id: {}".format(task_id))
@@ -78,7 +67,8 @@ class NCDCStationsInGeojson(Resource):
         logging.info("hms_controller.NCDCStationsInGeojson search completed.")
         logging.info("Adding data to mongoDB...")
         posts = mongo_db.posts
-        data = {'_id': task_id, 'data': stations}
+        time_stamp = datetime.utcnow()
+        data = {'_id': task_id, 'date': time_stamp, 'data': stations}
         posts.insert_one(data)
 
 
@@ -92,12 +82,23 @@ class NLDASGridCells(Resource):
 
     def get(self):
         args = parser.parse_args()
-        huc8 = args.huc_8_num
-        huc12 = args.huc_12_num
-        comid = args.com_id_num
-        if huc8 and comid:
-            catchment_cells = CatchmentGrid.getIntersectCellsInCatchment(huc8, comid)
-            return Response(catchment_cells)
-        elif huc12:
-            catchment_cells = CatchmentGrid.getIntersectCellsInHuc12(huc12)
-            return Response(catchment_cells)
+        task_id = self.start_async.apply_async(args=(args.huc_8_num, args.huc_12_num, args.com_id_num), queue="qed")
+        return Response(json.dumps({'job_id': task_id.id}))
+
+    @celery.task(name='hms_nldas_grid', bind=True, ignore_result=False)
+    def start_async(self, huc_8_id, huc_12_id, com_id):
+        task_id = celery.current_task.request.id
+        logging.info("task_id: {}".format(task_id))
+        logging.info("hms_controller.NLDASGridCells starting calculation...")
+        if huc_8_id and com_id:
+            catchment_cells = CatchmentGrid.getIntersectCellsInCatchment(huc_8_id, com_id)
+        elif huc_12_id:
+            catchment_cells = CatchmentGrid.getIntersectCellsInHuc12(huc_12_id)
+        else:
+            catchment_cells = {}
+        logging.info("hms_controller.NLDASGridCells calcuation completed.")
+        logging.info("Adding data to mongoDB...")
+        posts = mongo_db.posts
+        time_stamp = datetime.utcnow()
+        data = {'_id': task_id, 'date': time_stamp, 'data': catchment_cells}
+        posts.insert_one(data)
