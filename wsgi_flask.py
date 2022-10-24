afe96b8d87058ee3520f9caec59e5dc3e341a6a6
Deron Smith <deron.brock@gmail.com>
2022-10-24 14:20:36 -0400

    Trigger build for action test

--- wsgi_flask.py
+++ wsgi_flask.py
@@ -11,9 +11,4 @@ import flask_hms
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.wrappers import Response
 
-# flask_app = DispatcherMiddleware(
-#     Response('Page not found', status=404),
-#     {'/hms': flask_hms.app}
-# )
-
 application = flask_hms.app
