79c7d141e88272a830a2cef89821b05384dde4a9
Deron Smith <deron.brock@gmail.com>
2024-10-10 16:45:02 -0400

    shape statements to determine contents of files

--- modules/hms/locate_timezone.py
+++ modules/hms/locate_timezone.py
@@ -14,8 +14,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 tz_df = None
-tz_list = None
-if os.path.exists(r"/src/hms_flask/modules/hms/tz_files/tz_world.dbf"):
+if os.path.exists(r"/src/hms_flask/modules/hms/tz_files/tzinfo_updated.csv"):
     tz_df = pd.read_csv(r"/src/hms_flask/modules/hms/tz_files/tz_world.dbf", names=("TZID", "TZName", "TZOffset", "TZOffsetDST"))
 else:
     tz_list = os.path.join(os.path.realpath(__file__), "..", "tz_files", "tzinfo_updated.csv")
@@ -28,14 +27,15 @@ else:
 
 
 def get_timezone(latitude, longitude):
-    logger.info("TZ_DF: " + str(tz_df))
-    logger.info("TZ_LIST: " + str(tz_list))
+    print("TZ_DF: " + str(tz_df.shape))
+    print("TZ_dbf: " + str(tz_gdf.shape))
     tz_point = Point(float(longitude), float(latitude))
     gdf = tz_gdf[tz_gdf.contains(tz_point)]
     if gdf.empty:
         return json.dumps({"error": "No timezone found for the given coordinates"})
     tz_name = gdf['TZID'].values[0]
-    tz_offset = tz_df[tz_df["TZName"] == tz_name]["TZOffset"].values[0].split(":")[0]
+    tz_offset = tz_df[tz_df["TZName"] == tz_name]["TZOffset"]
+    tz_offset = tz_offset.values[0].split(":")[0]
     timezone_details = {"latitude": float(latitude), "longitude": float(longitude), "tzName": str(tz_name), "tzOffset": int(tz_offset)}
     logger.info(f"Timezone details: {timezone_details}")
     return timezone_details
